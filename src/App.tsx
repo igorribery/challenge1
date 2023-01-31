b60eeb206cc831f26e7308f15a5174242aa5124d
Igor <(igorrds1998@gmail.com)>
2023-01-31 20:51:44 -0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-type ListType = {
+type State = {
   x: number,
   y: number
 }
- 
 
 
 const App = () => {
 
-  const [list, setList] = useState<ListType[]>([]);
-  const [undid, setUndid] = useState<ListType[]>([]);
+  const [list, setList] = useState<State[]>([]);
+  const [undid, setUndid] = useState<State[]>([]);
 
+ 
   const handleClick = (e: React.MouseEvent<HTMLElement>) => {
 
     const newDot = {
@@ -20,39 +20,32 @@ const App = () => {
       y: e.clientY
     }
     setList((prev) => [...prev, newDot]);
-    setUndid([]);
+    setUndid([]);    
   }
+
   const handleUndo = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
 
-    if(list.length === 0) {
-      return;
-    }
+    const lastItem = list[list.length - 1];
 
-    const lastItem = list[list.length -1];
-    setUndid((prev) => [...prev, lastItem])
+    setUndid((prev) => [...prev, lastItem]);
 
-    setList((prev) => {
-      const newArr = [...prev].slice(0, -1);
-      return newArr;
-     
-    })
+    setList((prev) => [...prev].slice(0, -1));
+
   }
 
   const handleRedo = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
+      e.stopPropagation();
 
-    if(undid.length === 0) {
-      return;
-    }
+      if(undid.length === 0) {
+        return;
+      }
 
-    const recoveredDot = undid[undid.length -1];
+      const recoveryDot = undid[undid.length -1];
 
-    setUndid((prev) => {
-      const newArr = [...prev].slice(0, -1);
-      return newArr;
-    });
-    setList((prev) => [...prev, recoveredDot]);
+      setList((prev) => [...prev, recoveryDot]);
+
+      setUndid((prev) => [...prev].slice(0, -1));
 
   }
 
@@ -61,7 +54,7 @@ const App = () => {
       <button onClick={handleUndo}>Desfazer</button>
       <button onClick={handleRedo}>Refazer</button>
       {list.map((item, index) => (
-        <span className='dot' key={index} style={{left: item.x, top: item.y }} />
+        <span key={index} className='dot' style={{left: item.x, top: item.y}}></span>
       ))}
       
     </div>
